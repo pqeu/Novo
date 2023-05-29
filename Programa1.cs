cb438ae098f0d6121eac2998dfc7c1fb8d9837c7
pqeu <114673994+pqeu@users.noreply.github.com>
2023-05-29 20:03:02 -0300

    Add files via upload

---
+++ Programa1.cs
@@ -0,0 +1,42 @@
+﻿
+// Leitor de inputs do teclado 
+public class JewelCollector {
+
+  public static void Main() {
+  
+      bool running = true;
+  
+      do {
+  
+          Console.WriteLine("Enter the command: ");
+          string command = Console.ReadLine();
+  
+          if (command.Equals("quit")) {
+              running = false;
+          } else if (command.Equals("w")) {
+              
+          } else if (command.Equals("a")) {
+              
+          } else if (command.Equals("s")) {
+            
+          } else if (command.Equals("d")) {
+          
+          } else if (command.Equals("g")) {
+              
+          }
+      } while (running);
+      
+  }
+}
+
+
+
+
+
+class Map
+{
+  // criando mapa  
+  // inserindo elementos dentro do mapa 
+  
+  
+}
